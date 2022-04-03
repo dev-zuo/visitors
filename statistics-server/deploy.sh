b2c59d034274fe0c838c05175d13aef6b018751f
zuoxiaobai <guoqzuo@gmail.com>
2022-04-04 03:39:46 +0800

    chore: 更新部署脚本，更新 gitignore

--- statistics-server/deploy.sh
+++ statistics-server/deploy.sh
@@ -1,4 +1,5 @@
 git pull;
 npm run build;
 pm2 delete zuo-statistics;
-pm2 start dist/src/main.js -n zuo-statistics;
+# 注意打包路径，如果非 src 下有 ts 文件，nest 会改变打包方式
+pm2 start dist/main.js -n zuo-statistics;
